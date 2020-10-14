e63dd0976a1f69ae1bee4d539d29715a8408e6df
Bruno Willian Andrade <brunowillianandrade@gmail.com>
2020-10-14 18:17:58 -0300

    first commit 30min of work

---
+++ Assets/Scripts/Contador.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Contador : MonoBehaviour
+{
+
+    public int totalHits = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < totalHits; i++)
+        {
+            var go = Instantiate(Resources.Load<GameObject>("Prefabs/Defence/Tower"), transform.position, transform.rotation, transform.parent);
+        }   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
